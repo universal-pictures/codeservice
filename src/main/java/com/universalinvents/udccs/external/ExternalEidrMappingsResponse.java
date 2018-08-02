3c877751af4114857c8af59876fb94490c7a23e9
Kyle Kirkland <kyleki@gmail.com>
2018-08-02 14:53:32 -0700

    US-228: Added extra retailer filtering based on content availability from defined EST Inventory service(s)

---
+++ src/main/java/com/universalinvents/udccs/external/ExternalEidrMappingsResponse.java
@@ -0,0 +1,31 @@
+package com.universalinvents.udccs.external;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.io.Serializable;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ExternalEidrMappingsResponse implements Serializable {
+
+    private Long count;
+    private Long minInventoryCount;
+
+    public ExternalEidrMappingsResponse() {}
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public Long getMinInventoryCount() {
+        return minInventoryCount;
+    }
+
+    public void setMinInventoryCount(Long minInventoryCount) {
+        this.minInventoryCount = minInventoryCount;
+    }
+}
