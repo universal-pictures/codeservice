ce8a6df9bd96144279c116e07ffccafd9fd773ab
Kyle Kirkland <kyleki@gmail.com>
2018-09-27 16:20:04 -0700

    Allowed cascading of saving & merging from partners

--- src/main/java/com/universalinvents/udccs/partners/ReferralPartner.java
+++ src/main/java/com/universalinvents/udccs/partners/ReferralPartner.java
@@ -30,13 +30,13 @@ public class ReferralPartner {
     private String logoUrl;
     private String status;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(name = "partner_retailer",
                joinColumns = @JoinColumn(name = "partner_id", referencedColumnName = "id"),
                inverseJoinColumns = @JoinColumn(name = "retailer_id", referencedColumnName = "id"))
     private Set<Retailer> retailers;
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(name = "partner_studio",
                joinColumns = @JoinColumn(name = "partner_id", referencedColumnName = "id"),
                inverseJoinColumns = @JoinColumn(name = "studio_id", referencedColumnName = "id"))
